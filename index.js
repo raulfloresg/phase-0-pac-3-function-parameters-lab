de6b9f2bbae66907ebfa70508e4635ddde1f1859
Raul <raulflores33@gmail.com>
2022-03-13 23:51:13 -0700

    Comple assigment

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction(name) {
+    console.log ('Hi, my name is Aki')
+   
+}
+
+function introductionWithLanguage(name, language){
+
+  return  "Hi, my name is Aki and I am learning to program in Ember.js."
+  ==  "Hi, my name is Samip and I am learning to program in React."
+}
